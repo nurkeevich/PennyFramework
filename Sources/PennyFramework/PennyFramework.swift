63782a9d39242097af8ad44d55a7cb4c52ffd43f
Tilekbek Kadyrov <tilekbekkadyrov@gmail.com>
2020-06-06 16:25:45 -0500

    Added new class and unit test

--- Sources/PennyFramework/PennyFramework.swift
+++ Sources/PennyFramework/PennyFramework.swift
@@ -1,3 +1,24 @@
-struct PennyFramework {
-    var text = "Hello, World!"
+import SwiftUI
+
+public struct PennyFramework {
+    public struct MainTextField: View {
+        @State private var placeholder: String
+        @Binding var text: String
+        
+        public init(placeholder: String, text: Binding<String>) {
+            self._placeholder = State(initialValue: placeholder)
+            self._text = text
+        }
+        
+        public var body: some View {
+            HStack {
+                Image(systemName: "person").foregroundColor(Color.blue)
+                TextField(placeholder, text: $text)
+                    .font(.system(size: 20, weight: .bold, design: .default))
+                     .foregroundColor(Color.blue)
+            }
+            .padding()
+            .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.blue, lineWidth: 2))
+        }
+    }
 }
